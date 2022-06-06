5513683b4f1f326f25c2a4ebc8b38c52049ebcca
Your Name <your@email.com>
2022-06-06 13:26:42 -0500

    Added test

--- test/test_Calc.py
+++ test/test_Calc.py
@@ -4,12 +4,39 @@ import numpy.testing as npt
 import Calc
 
 def test_Calc_smoke():
-    #Smoke_test
-    obt = Calc.Calc_object()
+	#Smoke_test
+	obt = Calc.Calc_object()
 
 def test_Calc_object_fizz():
-    #test the fizz_function
-    obj = Calc.Calc_object()
-    output = obj.fizz()
+	#test the fizz_function
+	obj = Calc.Calc_object()
+	output = obj.fizz()
+	npt.assert_equal(output, "buzz")
 
-    npt.assert_equal(output, "buzz")
+def test_add():
+	result = Calc.add(3, 4)
+	assert result == 7
+
+def test_add_string():
+	with pytest.raises(TypeError):
+        	Calc.add("string", 4)
+
+def test_add_float():
+        with pytest.raises(TypeError):
+                Calc.add(1.1, 4)
+
+def test_mult():
+        result = Calc.mult(3, 4)
+        assert result == 12
+
+def test_mult_string():
+        with pytest.raises(TypeError):
+                Calc.mutl("string", 4)
+
+def test_mult_float():
+        with pytest.raises(TypeError):
+                Calc.mult(1.1, 4)
+
+
+
+
